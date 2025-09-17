4abc76af1872e397959b09636da6339d716edfde
ravvitkim <mirkim6086@naver.com>
2025-09-17 10:27:26 +0900

    중첩라우팅은 라우트 밑에 트리구조처럼 추가 해당컴포넌트는 Outlet으로보여줌

--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -1,38 +1,37 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import './AppNavBar.css'
 // Route : 페이지(컴포넌트) 이동 처리
-// Routes : url에 담겨있는 정보를 획득
-// Link : 실제로 페이지를 보여주는 역할,Link위치에 컴포넌트 뿌려줌
-import { Route, Routes, Link } from 'react-router-dom';
+// Routes : Route를 감싸는 용도
+// Link : 실제로 페이지를 보여주는 역할, Link위치에 컴포넌트 뿌려줌
+// userNavigate : 스크립트 영역에서 링크처리를 하는 훅
+import { Link, useNavigate } from "react-router-dom";
 
+function AppNavBar(){
 
-function AppNavbar(){
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className='App'>
+      <div className="App">
         <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand>Muzinjang</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link>
-              <Link to={"/"}>Home</Link>
-            </Nav.Link>
-            <Nav.Link><Link to={"/detail"}>Detail</Link></Nav.Link>
-            <Nav.Link><Link to={"/cart"}>Cart</Link></Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      {/* Routing 정보를 한꺼번에 모아놓는 장소 */}
-      {/* 스프링에서 사용하는 컨트롤러 클래스 */}
-      <Routes>
-        <Route path='/' element={<div>메인페이지</div>} />
-        <Route path='/detail' element={<div>상세페이지</div>} />
-        <Route path='/cart' element={<div>장바구니페이지</div>} />
-      </Routes>
+          <Container>
+            <Navbar.Brand>Muzinjang</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+              <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
+              <Nav.Link>Cart</Nav.Link>
+              <Nav.Link onClick={()=>{navigate('/about')}}>About</Nav.Link>
+              <NavDropdown title="Info" id="basic-nav-dropdown">
+                <NavDropdown.Item onClick={()=>{navigate('/about/member')}}>Member Page</NavDropdown.Item>
+                <NavDropdown.Item onClick={()=>{navigate('/about/location')}}>Location</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Container>
+        </Navbar>
       </div>
     </>
   )
 }
 
-export default AppNavbar;
+export default AppNavBar;
