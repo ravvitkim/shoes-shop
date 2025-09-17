217c3b3c996f6571079a800e0e44a4eb528f75a0
ravvitkim <mirkim6086@naver.com>
2025-09-17 11:11:43 +0900

    상세페이지 처리완료

--- src/Product/index.jsx
+++ src/Product/index.jsx
@@ -1,11 +1,18 @@
-function Product({shoes}){
-  const image = `/images/shoes${shoes.id +1}.jpg`;
+import { useNavigate } from "react-router-dom";
+
+function Product({ shoes }) {
+  const image = `/images/shoes${shoes.id + 1}.jpg`;
+  const navigate = useNavigate();
+
   return (
     <>
-    <img src={image}
-                    width="80%" />
-                  <h4>{shoes.title}</h4>
-                  <p>{shoes.content}</p>
+      <img onClick={() => (
+        navigate(`/detail/${shoes.id}`)
+      )}
+        src={image}
+        width="80%" />
+      <h4>{shoes.title}</h4>
+      <p>{shoes.content}</p>
     </>
   )
 }
