419eb66fca04ffa5b76b2185228efedcc4e2877c
ravvitkim <mirkim6086@naver.com>
2025-09-18 17:37:08 +0900

    데이터 가져오기 완료

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,11 +1,23 @@
 import {Container, Row, Col,Button} from 'react-bootstrap';
 import Product from '../Product';
 import axios from 'axios';
+import { useState } from 'react';
+import bg_png from "../assets/images/bg.png"
 
 function Home({product, setProduct}){
+  //몇 번 눌렀는지 체크하는 스테이트
+  let [dataLoadingCount, setDataLoadingCount] = useState(0)
+
+  let[loadingState,setLodingState] = useState(true)
+
+
   
   return(
     <div>
+      {/* 메인 대문사진 영역 시작 */}
+      <div className="main-bg" 
+        style={{backgroundImage: `url('${bg_png}')`}}  
+      />
     {/* 상품진열영역 시작 */}
       <Container>
         <Row xs={3}>
@@ -22,30 +34,59 @@ function Home({product, setProduct}){
         </Row>
       </Container>
       {/* 상품진열영역 끝 */}
+      {/* 로딩메세지 */}
+      <div className='text-center my-3'>
+          {loadingState && <div>Loading....</div>}
+      </div>
+
       <div className='d-flex justify-content-center
         align-items-center'>
-          <Button varient="primary" size="lg"
+          <Button variant="primary" size="lg"
           onClick={async()=>{
-            // try{
-            // const result1 = await axios('https://zzzmini.github.io/js/react_data_01.json')
-            // let temp = [...product, ...result1.data];
-            // setProduct(temp);
-            // }catch(error){
-            //   console.log("가져오기 실패",error)
-            // }
-            //데이터를 3개 가져오는 함수
-            axios
-            .get('https://zzzmini.github.io/js/react_data_01.json')
-            .then((result)=>{
-              let temp = [...product]
-              for(let x of result.data){
-                temp.push(x)
-              }
-              setProduct(temp)
-            })
-            .catch(()=>{
-              console.log("가져오기 실패")
-            })
+            let getUrl = ''
+            if(dataLoadingCount == 0){
+              getUrl = 'https://zzzmini.github.io/js/react_data_01.json'
+              setDataLoadingCount(dataLoadingCount +1);
+              setLodingState(true)
+            }
+            else if(dataLoadingCount === 1){
+              getUrl = 'https://zzzmini.github.io/js/react_data_02.json'
+              setDataLoadingCount(dataLoadingCount +1);
+              setLodingState(true)
+            }else{
+              alert('데이터가 존재하지 않아요')
+              return;
+            }
+
+
+            try{
+            const result = await axios(getUrl)
+            let temp = [...product, ...result.data];
+            setProduct(temp);
+            }catch(error){
+              console.log("가져오기 실패",error)
+            }finally{
+              setLodingState(false)
+            }
+
+
+
+
+
+
+            // 데이터를 3개 가져오는 함수
+            // axios
+            // .get('https://zzzmini.github.io/js/react_data_01.json')
+            // .then((result)=>{
+            //   let temp = [...product]
+            //   for(let x of result.data){
+            //     temp.push(x)
+            //   }
+            //   setProduct(temp)
+            // })
+            // .catch(()=>{
+            //   console.log("가져오기 실패")
+            // })
           }}
           >데이터 가져오기</Button>
 
